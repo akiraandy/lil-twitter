b37a9533cae24a954528a24163ffc9d69f7fa633
akiraandy <andypschwartz@gmail.com>
2017-12-11 23:15:37 -0600

    create comment partial and create find_commentable method in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :find_commentable
 
   def new
     @comment = Comment.new
   end
 
   def create
-    @comment = Comment.create(comment_params)
-    @comment.save!
+    @comment = @commentable.comments.new comment_params
+    if @comment.save
+      redirect_to :back
+    else
+      redirect_to :back
+    end
   end
 
   private
 
   def comment_params
     @comment.require(:comment).permit(:body)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Tweet.find_by_id(params[:tweet_id]) if params[:tweet_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :comments
   end
+
+  resources :comments do
+    resources :comments
+  end
 end
